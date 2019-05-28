7177b7e17d5b2036f1229bf3d222e47ac16b9631
Emely <mly.molina.minaya@gmail.com>
2019-05-28 15:39:28 -0700

    removed logo and some css styles, created function to initialize and render state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import FamilyTree from './FamilyTree'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      Loading...
+      <FamilyTree/>
     </div>
   );
 }
---
+++ src/FamilyTree.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { TreeNode } from './TreeNode'
+
+export default class FamilyTree extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      initialized: false,
+      familyTree: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState(this._initializeFamilyTree())
+  }
+
+  render() {
+    return(
+      <div>
+        {!(this.state.initialized) && (
+          <div>Loading...</div>
+        )}
+        {this.state.initialized && (
+          <div>{this._renderTree(this.state.familyTree)}</div>
+        )}
+      </div>
+    )
+  }
+
+  _initializeFamilyTree() {
+    const tree = new TreeNode({ name: 'Nona Rosa', age: 79 })
+
+    tree.children.push(new TreeNode({ name: 'Antonio' }))
+    tree.children.push(new TreeNode({ name: 'Gustavo' }))
+    tree.children.push(new TreeNode({ name: 'Isabella' }))
+
+    tree.children[0].children.push(new TreeNode({ name: 'Emely', married: true, dogs: true }))
+    tree.children[0].children.push(new TreeNode({ name: 'Luanny', married: false }))
+    tree.children[0].children.push(new TreeNode({ name: 'Carolina', married: true }))
+
+    return {initialized: true, familyTree: tree}
+  }
+
+  _renderTree(TreeNode) {
+    return(
+      <div className='TreeNode'>
+        <h3>
+          {' '}{TreeNode.name}
+        </h3>
+        {TreeNode.children && TreeNode.chidlren > 0 &&
+          <ul>
+            {TreeNode.children.map((child, index) => <li key={index}>{this._renderTree(child)}</li>)}
+          </ul>
+        }
+      </div>
+    )
+  }
+}
